ecf32627d825bed659444dd4a11892e708e63095
moharick <moharick@gmail.com>
2019-10-03 12:42:19 +0300

    Deployment

--- tribune/settings.py
+++ tribune/settings.py
@@ -42,7 +42,7 @@ else:
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = ['moha-tribune.herokuapp.com']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
